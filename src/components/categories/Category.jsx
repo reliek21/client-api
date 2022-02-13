ae5efefd7d91b26e5570dde54391e1d26bd61695
reliek21 <keilerguardo@gmail.com>
2022-02-13 00:54:45 -0500

    Changing icons

--- src/components/categories/Category.jsx
+++ src/components/categories/Category.jsx
@@ -15,8 +15,8 @@ const SingleCategory = ({ category }) => {
             <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900">{name}</td>
             <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900">{description}</td>
             <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900">{active.toString()}</td>
-            <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900"><button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Edit</button></td>
-            <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900"><button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Delete</button></td>
+            <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900"><button class="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"><i className="bi bi-pencil-square"></i></button></td>
+            <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900"><button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"><i className="bi bi-trash3"></i></button></td>
         </tr>
     )
 }
--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -19,8 +19,8 @@ const SingleProduct = ({ product }) => {
             <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900">{brand}</td>
             <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900">{category.name}</td>
             <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900">${price}</td>
-            <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900"><button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Edit</button></td>
-            <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900"><button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Delete</button></td>
+            <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900"><button class="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"><i className="bi bi-pencil-square"></i></button></td>
+            <td className="border-t-2 border-gray-200 px-4 py-3 text-sm text-gray-900"><button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"><i className="bi bi-trash3"></i></button></td>
         </tr>
     )
 }

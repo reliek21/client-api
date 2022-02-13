941873f3ece3369e4c13c3c21311ef13cf5c30c2
reliek21 <keilerguardo@gmail.com>
2022-02-13 05:19:39 -0500

    Last Update for Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import Categories from "./components/layout/Categories";
 
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // layout
 import Product from "./components/layout/Products";
@@ -27,14 +27,7 @@ function App() {
                     <Route exact path="/category/edit/:id" component={EditCategory} />
                 </Switch>
                 <Categories />
-                <footer className="text-gray-600 body-font">
-                    <div className="px-5 py-8 mx-auto flex items-center sm:flex-row bg-gray-100">
-                        <a href="https://github.com/reliek21" className="flex mx-auto title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                            <p className="ml-3">Create by:</p>
-                            <span className="ml-3 text-xl">Keiler Guardo</span>
-                        </a>
-                    </div>
-                </footer>
+
             </Fragment>
         </BrowserRouter>
     );
